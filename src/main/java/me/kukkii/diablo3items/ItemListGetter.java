39fd01f521746ac5c4524301ad8d0af8be958fbd
KAZUOMI KASHII <kazuomi@sfo-kaz.healthline.com>
2012-06-26 14:12:31 -0700

    Tests ItemListGetter. It fails parsing the content of the 3rd URL.

--- src/main/java/me/kukkii/diablo3items/ItemListGetter.java
+++ src/main/java/me/kukkii/diablo3items/ItemListGetter.java
@@ -2,20 +2,24 @@ package me.kukkii.diablo3items;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 import java.net.URL;
 import java.io.PrintWriter;
 import java.io.OutputStreamWriter;
 
 public class ItemListGetter{
 
+  private static final String FILENAME = "data/itemdata.txt";
+
   public List<String> getUrls() throws Exception{
 
     List<String> urls = new ArrayList<String>();
-    FileReader fr = new FileReader("/Users/cookieahaha/git/diablo3items/data/itemdata.txt");
+    FileReader fr = new FileReader(FILENAME);
     BufferedReader br = new BufferedReader(fr);
 
     String input = null;
@@ -32,10 +36,14 @@ public class ItemListGetter{
     ItemListGetter getter = new ItemListGetter();
     ItemParser ip = new ItemParser();
     PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out, "UTF-8"));
+    int i = 1;
     for(String url : getter.getUrls()){
-      for (Item item : ip.parse(getter.getContent(url))){
+      String content = getter.getContent(url);
+      getter.save(content, "data/" + (i++) + ".html");
+      for (Item item : ip.parse(content)) {
         out.println(item);
       }
+      out.flush();
     }
     out.flush();
   }
@@ -45,4 +53,7 @@ public class ItemListGetter{
       return input;
   }
 
+  public void save(String content, String filename) throws Exception {
+    FileUtils.writeStringToFile(new File(filename), content, "UTF-8");
+  }
 }
