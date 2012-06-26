9cc10b1a83c95f0c874b04b83da07c129c356dd3
Yasukazu Kashii <cookieahaha@gmail.com>
2012-06-26 02:25:43 -0700

    itemlistgetter2

--- src/main/java/me/kukkii/diablo3items/ItemListGetter.java
+++ src/main/java/me/kukkii/diablo3items/ItemListGetter.java
@@ -5,6 +5,10 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.io.IOUtils;
+import java.net.URL;
+import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
 
 public class ItemListGetter{
 
@@ -16,6 +20,9 @@ public class ItemListGetter{
 
     String input = null;
     while ((input = br.readLine()) != null){
+        if(input.charAt(0) == '#'){
+          continue;
+        }
     	urls.add(input);
     }      
     return urls;
@@ -23,9 +30,19 @@ public class ItemListGetter{
 
   public static void main(String[] args) throws Exception{
     ItemListGetter getter = new ItemListGetter();
+    ItemParser ip = new ItemParser();
+    PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out, "UTF-8"));
     for(String url : getter.getUrls()){
-      System.out.println(url);
-    } 
- }
+      for (Item item : ip.parse(getter.getContent(url))){
+        out.println(item);
+      }
+    }
+    out.flush();
+  }
+
+  public String getContent(String url) throws Exception{
+      String input = IOUtils.toString(new URL(url));
+      return input;
+  }
 
 }
